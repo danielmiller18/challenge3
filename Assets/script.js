683b1cf6e629aae0a2cf4e5f822ab1bba4ffdfc5
Daniel Miller <104872554+danielmiller18@users.noreply.github.com>
2022-10-02 18:32:17 -0500

    init

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,10 @@
-3// Assignment Code
-
-// console.log(isNaN(10)) // false
-// console.log(isNaN("abc")) // true
-
 var generateBtn = document.querySelector("#generate");
 
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz"
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var numberChar = "0123456789"
-var specialChar = "!@#$%^&*()"
+var specialChar = "!@#$%^&*()-+?_"
 
 var passwordLength;
 var uppercaseCheck;
@@ -29,7 +24,7 @@ function checkLength() {
     alert("Password length must in between 8 - 128")
     checkLength()
   }
-  // // if the input is not a number
+
   else if (isNaN(passwordLength)) {
     alert("Please enter a valid number!")
     checkLength()
@@ -42,12 +37,8 @@ function checkLength() {
   return passwordLength
 }
 
-
-// checkLength()
-
-// // function that will be used to check whether the user wants to include uppercase or not
 function checkUppercase() {
-  uppercaseCheck = prompt("Do you want to add uppercase letters in your password");
+  uppercaseCheck = prompt("Do you want to add uppercase letters in your password (yes/no)");
 
   uppercaseCheck = uppercaseCheck.toLowerCase();
 
@@ -57,11 +48,12 @@ function checkUppercase() {
   }
   else if (uppercaseCheck === "yes") {
     uppercaseCheck = true;
-    checkUppercase();
+    return uppercaseCheck;
   }
   else if (uppercaseCheck === "no") {
     uppercaseCheck = false;
-    checkUppercase();
+    return uppercaseCheck;
+
   }
   else {
     alert("You can only write yes/no")
@@ -71,9 +63,103 @@ function checkUppercase() {
   return uppercaseCheck
 }
 
+function checkNumber() {
+  numberCheck = prompt("Do you want to include the numbers in your password (yes/no)");
+
+  if (numberCheck === null || numberCheck === "") {
+    alert("Please Give a Yes/No")
+    checkNumber();
+  }
+  else if (numberCheck === "yes") {
+    numberCheck = true;
+   return numberCheck
+  }
+  else if (numberCheck === "no") {
+    numberCheck = false;
+    return numberCheck
+   
+  }
+  else {
+    alert("You can only write yes/no")
+  }
 
 
-// Write password to the #password input
+  return numberCheck
+}
+
+function checkSpecial() {
+  specialCheck = prompt("Do you want to add special characters in your password (yes/no)");
+
+  specialCheck = specialCheck.toLowerCase();
+
+  if (specialCheck === null || specialCheck === "") {
+    alert("Please Give a Yes/No")
+    checkSpecial();
+  }
+  else if (specialCheck === "yes") {
+    specialCheck = true;
+    return specialCheck
+  }
+  else if (specialCheck === "no") {
+    specialCheck = false;
+    return specialCheck
+    
+  }
+  else {
+    alert("You can only write yes/no")
+  }
+
+
+  return specialCheck
+} 
+
+function generatePassword(){
+    checkLength();
+    console.log(passwordLength);
+
+    checkUppercase()
+    console.log(uppercaseCheck)
+
+    checkNumber()
+    console.log(numberCheck)
+
+    checkSpecial()
+    console.log(specialCheck)
+
+   
+
+    var characters = lowercaseChar;
+    var password = ""
+
+    if(uppercaseCheck && numberCheck && specialCheck){
+      characters += uppercaseChar + numberChar + specialChar;
+    } else if(uppercaseCheck && numberCheck){
+      characters += uppercaseChar + numberChar;
+    } else if(numberCheck && specialCheck){
+      characters += uppercaseChar + numberChar;
+    }
+    else if(uppercaseCheck && specialCheck){
+      characters += uppercaseChar + specialChar
+    } else if(uppercaseCheck){
+      characters += uppercaseChar;
+    } else if(numberCheck){
+      characters += numberChar
+    } else if(specialCheck){
+        characters += specialChar
+    }
+    else {
+      characters === lowercaseChar;
+    }
+
+
+    for(var i = 0; i < passwordLength; i++){
+      password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return password
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -82,5 +168,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
